13f1bfca4a068d67ea03ae9d76b083ff88fc9f31
prog <macuser@vedro-d.(none)>
2011-03-24 17:33:17 +0300

    asf

--- app/models/item/image.rb
+++ app/models/item/image.rb
@@ -1,14 +1,18 @@
 class Item::Image < Asset
-  has_attached_file :attachment, :styles => {:thumb => "128x80#", :normal => "400x300"}
+  has_attached_file :attachment, :styles => {:thumb => "128x80#", :normal => "400x300", :masked => ["128x80#", :png]}
   after_save :mask
+  
+  #def masked_url
+  #  self.attachment.url(:thumb)+".png"
+  #end
   
   def mask
 
 #    cmd = self.attachment.path(:thumb)+' '+Rails.root.to_s+
 #        '/public/images/mask.png'+' -alpha Off -compose Copy_Opacity -composite '+self.attachment.path(:thumb)
-    cmd = "'#{self.attachment.path(:thumb)}'" + ' -alpha set -gravity center -extent 135x80'
+    cmd = "'#{self.attachment.path(:masked)}'" + ' -alpha set -gravity center -extent 135x80'
     cmd += ' '+"'"+Rails.root.to_s+'/public/images/mask2.png'+"'"+' -compose DstIn -composite '
-    cmd += "'"+self.attachment.path(:thumb)+".png"+"'"
+    cmd += "'"+self.attachment.path(:masked)+".png"+"'"
 #    logger.info cmd
 #    puts cmd
     
