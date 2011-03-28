469684d548c7a10136efc20c387a9af181945016
prog <macuser@vedro-d.(none)>
2011-03-28 21:16:29 +0400

    some_more

--- app/models/item/specification_image.rb
+++ app/models/item/specification_image.rb
@@ -1,3 +1,4 @@
 class Item::SpecificationImage < Asset
-  has_attached_file :attachment, :styles => {:thumb => "130x100"}
+  has_attached_file :attachment, :styles => {:thumb => "130x100", :normal => "400x300#", :big => "1024x768", 
+    :small_wallpaper => "1024x768!", :medium_wallpaper => "1280x800!", :big_wallpaper => "1280x1024!"}
 end
