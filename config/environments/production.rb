81f72ba92b9f31778e7711ee8c021932fa91f106
prog <macuser@vedro-d.(none)>
2011-04-04 18:48:23 +0400

    mond

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,8 @@ Navigator2::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+  
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
