66c01fe2d809d1f72e0a99aaf81138f3a6f6b29f
prog <macuser@vedro-d.(none)>
2011-04-06 20:10:10 +0400

    qwer

--- vendor/plugins/awesome_nested_set/lib/awesome_nested_set.rb
+++ vendor/plugins/awesome_nested_set/lib/awesome_nested_set.rb
@@ -434,6 +434,7 @@ module CollectiveIdea #:nodoc:
         # the base ActiveRecord class, using the :scope declared in the acts_as_nested_set
         # declaration.
         def nested_set_scope
+          options = {}
           scopes = Array(acts_as_nested_set_options[:scope])
           options[:conditions] = scopes.inject({}) do |conditions,attr|
             conditions.merge attr => self[attr]
