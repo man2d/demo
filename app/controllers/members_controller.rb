2b8da05902d1abed02c8611b748c07c287902f00
prog <macuser@vedro-d.(none)>
2011-04-07 20:11:18 +0400

    hz_che_kak

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,6 +2,6 @@ class MembersController < ApplicationController
   before_filter :authenticate_user!
   def index
     @page = Page.find_by_slug("members")
-    @users = User.where(:activated => true).all
+    @members = User.where(:activated => true).all
   end
 end
