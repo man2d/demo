ae3158cea98ebcb5b182001766745fa8fe248bb6
prog <macuser@vedro-d.(none)>
2011-04-14 17:43:10 +0400

    smth

--- app/models/item/video.rb
+++ app/models/item/video.rb
@@ -1,12 +1,5 @@
 class Item::Video < Asset
-  has_attached_file :attachment, :styles => {
-          :avi => {:size => '100x100', :format => 'avi'},
-          :thumbnail  => {:size => '150x100', :format => 'jpg', 
-            :disable_audio => true, :rate => 1, :video_frames => 1, :force_format => 'image2'}
-        
-},
-        :processors => [:transcode]
-        
+  has_attached_file :attachment
 #  process_in_background :attachment
 =begin
   :mov        => {:size => '320x240', :format => 'mov'},
@@ -14,5 +7,14 @@ class Item::Video < Asset
   :thumbnail  => {:size => '150x100', :format => 'jpg', 
     :disable_audio => true, :rate => 1, :video_frames => 1, :force_format => 'image2'}
 },
+
+, :styles => {
+        :avi => {:size => '100x100', :format => 'avi'},
+        :thumbnail  => {:size => '150x100', :format => 'jpg', 
+          :disable_audio => true, :rate => 1, :video_frames => 1, :force_format => 'image2'}
+      
+},
+      :processors => [:transcode]
+
 =end 
 end
